8a9b893feed9ba4c0ce02116d6b14ed291630313
sathish <ysathish1234@gmail.com>
2025-03-07 00:26:47 +0530

    06/03/2025

---
+++ src/main/java/com/nt/model/InventoryModel.java
@@ -0,0 +1,56 @@
+package com.nt.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class InventoryModel 
+{
+	private Long product_id;
+	private String product_name;
+	private float price;
+	private int quantity;
+	
+	public InventoryModel() {
+		super();
+	}
+	public InventoryModel(Long product_id,String product_name, float price, int quantity) {
+		super();
+		this.product_id=product_id;
+		this.product_name = product_name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	public Long getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(Long product_id) {
+		this.product_id = product_id;
+	}
+	public String getProduct_name() {
+		return product_name;
+	}
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	@Override
+	public String toString() {
+		return "InventoryModel [product_id=" + product_id + ", product_name=" + product_name + ", price=" + price
+				+ ", quantity=" + quantity + "]";
+	}
+	
+	
+
+}
